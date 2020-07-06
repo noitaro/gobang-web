6b12d429ba5f47006c80a34d30217d83ec750804
Your Name <you@example.com>
2020-07-06 20:19:48 +0900

    aaa

--- src/settings.9437f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","gobang"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"c95eb"}};
